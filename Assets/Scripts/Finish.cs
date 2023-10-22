55d1bc0aa410cb8fa290811e39671ad536c538c5
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2023-10-22 20:39:39 +0300

    Menu

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    public string nextLevelName;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Contains("Player"))
+        {
+            SceneManager.LoadScene(nextLevelName);
+        }
+    }
+}
